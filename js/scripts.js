4464e4a4b8a30327e7c5d289e03ed4ec26f98fa2
shonburgundy <seanbeloussow@yahoo.com>
2020-04-10 12:26:44 -0700

    add for loop

--- js/scripts.js
+++ js/scripts.js
@@ -7,25 +7,30 @@ function Pizza(pizzaSize, pizzaToppings, pizzaType) {
 }
 //Function will adjust cost based on user input
 Pizza.prototype.costAdjust = function() {
+  var cost = 10;
   if (this.pizzaSize === "large") {
-    this.pizzaCost += 8;
+    cost += 8;
   } else if (this.pizzaSize === "medium") {
-    this.pizzaCost += 4;
+    costs += 4;
   } //this.pizzaSize === "small" pizzaCost will be standard value
 
   if (this.pizzaType === "type") {
-    this.pizzaCost += 8;
-  } 
-  if (this.pizzaToppings === "veggie") {
-    this.pizzaCost += 1;
-  } 
-  if (this.pizzaToppings === "meat") {
-    this.pizzaCost += 2;
+    cost += 8;
   }
-  if (this.pizzaToppings === "extra") {
-    this.pizzaCost += 3;
+  for (var i = 0; i < this.length; i++) {
+  if (this === "veggie") {
+    cost += 1;
+    console.log(this, cost);
   } 
-  return this.pizzaCost;  
+  if (this === "meat") {
+    cost += 2;
+  }
+  if (this === "extra") {
+    cost += 3;
+  }
+}
+  this.pizzaCost = cost
+   return cost;  
 }
 
 //UI Logic
@@ -33,8 +38,11 @@ $(document).ready(function() {
   $("form#pizza-selections").submit(function(event) {
     event.preventDefault();
     var pizzaSize = $("input:checkbox[name=size-selection]:checked").val();
-    var pizzaToppings = $("input:checkbox[name=topping-selection]:checked").val();
-    var pizzaType =$("input:checkbox[name=type-selection]:checked").val();
+    var pizzaToppings = [];
+    $.each($("input[name=topping-selection]:checked"), function() {
+      pizzaToppings.push($(this).val());
+    });
+    var pizzaType = $("input:checkbox[name=type-selection]:checked").val();
 
     var pizza = new Pizza(pizzaSize, pizzaToppings, pizzaType);
     pizza.costAdjust();

ac9331ded259fb9f3095ace1c82ef63ac59d4ddb
shonburgundy <seanbeloussow@yahoo.com>
2020-04-10 09:16:33 -0700

    add Html bones, add pizza constructor & costAdjust function

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//Business Logic
+function Pizza(pizzaSize, pizzaToppings) {
+  this.pizzaSize = pizzaSize;
+  this.pizzaToppings = pizzaToppings;
+  this.pizzaCost = 10
+}
+//Function will adjust cost based on user input
+Pizza.prototype.costAdjust = function() {
+  if (this.pizzaSize === "large") {
+    this.pizzaCost += 8;
+  } else if (this.pizzaSize === "medium") {
+    this.pizzaCost += 4;
+  } //this.pizzaSize === "small" pizzaCost will be standard value
+
+  if (this.pizzaToppings === "veggie") {
+    this.pizzaCost += 1;
+  } else if (this.pizzaToppings === "meat") {
+    this.pizzaCost += 2;
+  }
+  return this.pizzaCost;  
+}

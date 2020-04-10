097f92c3eb23c2294c9ece9dde7d548e7f8512fd
shonburgundy <seanbeloussow@yahoo.com>
2020-04-10 12:26:25 -0700

    add ui logic for output refactor if statements, add for loop for toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //Business Logic
-function Pizza(pizzaSize, pizzaToppings) {
+function Pizza(pizzaSize, pizzaToppings, pizzaType) {
   this.pizzaSize = pizzaSize;
   this.pizzaToppings = pizzaToppings;
+  this.pizzaType = pizzaType;
   this.pizzaCost = 10
 }
 //Function will adjust cost based on user input
@@ -12,14 +13,32 @@ Pizza.prototype.costAdjust = function() {
     this.pizzaCost += 4;
   } //this.pizzaSize === "small" pizzaCost will be standard value
 
-  if (this.pizzaToppings === "type") {
+  if (this.pizzaType === "type") {
     this.pizzaCost += 8;
-  } else if (this.pizzaToppings === "veggie") {
+  } 
+  if (this.pizzaToppings === "veggie") {
     this.pizzaCost += 1;
-  } else if (this.pizzaToppings === "meat") {
+  } 
+  if (this.pizzaToppings === "meat") {
     this.pizzaCost += 2;
-  } else if (this.pizzaToppings === "extra") {
-    this.pizzaCost += 3;
   }
+  if (this.pizzaToppings === "extra") {
+    this.pizzaCost += 3;
+  } 
   return this.pizzaCost;  
 }
+
+//UI Logic
+$(document).ready(function() {
+  $("form#pizza-selections").submit(function(event) {
+    event.preventDefault();
+    var pizzaSize = $("input:checkbox[name=size-selection]:checked").val();
+    var pizzaToppings = $("input:checkbox[name=topping-selection]:checked").val();
+    var pizzaType =$("input:checkbox[name=type-selection]:checked").val();
+
+    var pizza = new Pizza(pizzaSize, pizzaToppings, pizzaType);
+    pizza.costAdjust();
+    $("#output").html("Your total is $" + pizza.pizzaCost)
+    console.log(pizza);
+  })
+});
